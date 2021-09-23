a30e6ad932e112fb948fe9071d579d9be1a31d7d
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-09-23 18:39:31 +0530

    completed todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,78 +1,95 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
-    constructor(props){
-        super(props);
-        this.state =
-        {
-             tasks : [{id:1,txt:"text1"},{id:2,txt:"text2"},{id:3,txt:"text3"}],
-            currentTask : ''
+    constructor()
+    {
+        super();
+        this.state={
+            tasks:[{id:1,txt:'First task'},{id:2,txt:'Second Task'},
+            {id:3,txt:'Third Task'}],
+           
         }
+       
     }
-    handleChange = (e) => {
-        this.setState({currentTask:e.target.value});
+  
+    handleClick=(task)=>{
+       
+        let nta = [...this.state.tasks,
+            {id:this.state.tasks.length+1,txt:task}];
+        this.setState({
+            tasks:nta
+        })
+       
     }
-    onSubmit = () =>{
-     //   this.state.tasks.push({id:this.state.tasks.length+1,txt : this.state.currentTask});
-      // this.state.currentTask = '';
-    let newArr = [...this.state.tasks,{id:this.state.tasks.length+1,txt:this.state.currentTask}];
-    this.setState({
-        tasks:newArr,
-        currentTask : ''
-    })
-    }
-    onDelete = (id) =>{
-        let newAra = this.state.tasks.filter(task =>
-        {
-            return task.id!=id;
-    })
-    this.setState({
-        tasks:newAra
-    })
+    onDelete =(id)=>{
+        let nta = this.state.tasks.filter(obj=>{
+            return obj.id!=id
+        })
+        this.setState({
+            tasks:nta
+        })
+
     }
     render() {
-       
+
         return (
-                <>
-                <InputComponent value = {this.state.currentTask} onChange = {this.handleChange} onSubmit = {this.onSubmit}/>
-                <TaskList tasks = {this.state.tasks} onDelete = {this.onDelete}/>
-                </>
-            
+            <>
+            <InputComponent  handleClick={this.handleClick} />
+            <TaskList tasks={this.state.tasks} 
+            onDelete={this.onDelete} />
+            </>
         )
     }
 }
+
 class InputComponent extends Component {
     constructor(props)
     {
         super(props);
+        this.state={
+            currTask:''
+        }
+    }
+    handleChange=(e)=>{
+        this.setState({currTask:e.target.value})
     }
     render() {
         return (
-            <div className = "InputContainer">
-        <input type = "text" value = {this.props.value} onChange = {this.props.onChange}></input>
-        <button onSubmit = {this.props.onSubmit}>add</button>
-            </div>
+            <div className='input-container'>
+            <input  type='text' value={this.state.currTask}
+             onChange={this.handleChange} ></input>
+             {
+                 this.state.currTask ?
+            <button onClick={()=>{
+                this.props.handleClick(this.state.currTask);
+                this.setState({currTask:''})
+            }} >
+                Add</button>:<></>
+    }
+       </div>
         )
     }
 }
 class TaskList extends Component {
     constructor(props)
     {
-    super(props);
+        super(props);
     }
     render() {
         return (
-<div className = 'tasks-container'>
-                        <ul>
-                            {this.props.tasks.map(task => (
-                               <li key = {task.id}>
-                                    <h1>{task.txt}</h1>
-                                    <button onClick  = {()=>this.props.onDelete(task.id)}>delete</button>
-                                </li> 
-                            ))}
-                        </ul>
-                </div>       
-                 )
+            <div className='class-list'>
+                <ul>
+                    { 
+                        this.props.tasks.map(task=>(
+                            <li key={task.id}>
+                                <h1>{task.txt}</h1>
+                                <button 
+  onClick={()=>this.props.onDelete(task.id)} >Delete</button>
+                            </li>
+        ))
+                    }
+                </ul>    
+            </div>      
+        )
     }
 }
-
